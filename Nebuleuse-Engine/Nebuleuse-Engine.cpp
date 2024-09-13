7476901457c42ee79c032c0313a842afc0b453dc
Julian.B <julianboi.pro@gmail.com>
2024-09-13 19:58:18 +0200

    Initial commit with project structure

---
+++ Nebuleuse-Engine/Nebuleuse-Engine.cpp
@@ -0,0 +1,12 @@
+﻿// Nebuleuse-Engine.cpp : définit le point d'entrée de l'application.
+//
+
+#include "Nebuleuse-Engine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
