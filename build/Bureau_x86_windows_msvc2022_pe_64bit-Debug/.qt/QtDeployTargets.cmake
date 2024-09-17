0008319ba14d30c14a5c0d0889f87722a0ebc274
Julian.B <julianboi.pro@gmail.com>
2024-09-17 13:34:00 +0200

    Migrate QtCreator 14.0.1 and add few menu into window

---
+++ build/Bureau_x86_windows_msvc2022_pe_64bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_NebuleuseEngine_FILE H:/Bureau/NebuleuseEngine/build/Bureau_x86_windows_msvc2022_pe_64bit-Debug/NebuleuseEngine.exe)
+set(__QT_DEPLOY_TARGET_NebuleuseEngine_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_NebuleuseEngine_RUNTIME_DLLS D:/QtCreator/6.8.0/msvc2022_64/bin/Qt6Widgetsd.dll;D:/QtCreator/6.8.0/msvc2022_64/bin/Qt6Guid.dll;D:/QtCreator/6.8.0/msvc2022_64/bin/Qt6Cored.dll)
---
+++ build/Bureau_x86_windows_msvc2022_pe_64bit-Debug/vcpkg-dependencies/toolchain.cmake
@@ -0,0 +1,7 @@
+
+        set(CMAKE_C_COMPILER "C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/bin/HostX64/x64/cl.exe")
+        set(CMAKE_CXX_COMPILER "C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/bin/HostX64/x64/cl.exe")
+        
+        set(VCPKG_TARGET_TRIPLET x64-windows)
+        include("C:/Program Files/Microsoft Visual Studio/2022/Community/VC/vcpkg/scripts/buildsystems/vcpkg.cmake")
+      
